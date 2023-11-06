ecf213ac537f3c5fcb8b0a3fc8791540644da317
EC2 Default User <ec2-user@ip-172-31-46-71.ap-northeast-1.compute.internal>
2023-11-06 08:56:01 +0000

    [FINISH]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' =>'lists#index'
   
   # .../list/1や.../list/3に該当する
   get 'lists/:id' => 'lists#show', as:'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id/' => 'lists#update', as: 'update_list'
 end
